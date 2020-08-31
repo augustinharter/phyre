beff7bfa571246cd5a7f06efef3dd3f250ba2704
Augustin Harter <aharter@techfak.uni-bielefeld.de>
2020-08-31 17:53:13 +0200

    trained pyramid and fully

--- train_flownets.sh
+++ train_flownets.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-EPOCHS=2
+EPOCHS=50
 
-python flownet.py pyramid-GT -train -pyramid --epochs $EPOCHS
-#python flownet.py lin-GT -train -linear --epochs $EPOCHS
+python flownet.py pyramid-END -train -pyramid -test --epochs $EPOCHS --train_mode END
+python flownet.py pyramid-CONS -train -pyramid -test --epochs $EPOCHS --train_mode CONS
+python flownet.py pyramid-COMB -train -pyramid -test --epochs $EPOCHS --train_mode COMB
+python flownet.py pyramid-MIX -train -pyramid -test --epochs $EPOCHS --train_mode MIX
+python flownet.py pyramid-GT -train -pyramid -test --epochs $EPOCHS --train_mode GT
+
+python flownet.py linear-END -train -linear -test --epochs $EPOCHS --train_mode END
+python flownet.py linear-CONS -train -linear -test --epochs $EPOCHS --train_mode CONS
+python flownet.py linear-COMB -train -linear -test --epochs $EPOCHS --train_mode COMB
+python flownet.py linear-MIX -train -linear -test --epochs $EPOCHS --train_mode MIX
+python flownet.py linear-GT -train -linear -test --epochs $EPOCHS --train_mode GT
 
 #python flownet.py sequ-END -train -sequ -test --epochs $EPOCHS --train_mode END
 #python flownet.py sequ-CONS -train -sequ -test --epochs $EPOCHS --train_mode CONS
